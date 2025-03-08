92b4a1fa8d3eecfc57548cd6e6b6c52a090609d3
NextPage02 <moshiur.r.nafi@gmail.com>
2025-03-08 11:19:48 +0600

    added firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dash_n_go/ui/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Scaffold(),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
